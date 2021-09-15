d3d4487eced2461358fe58c168f502957fdbf5cb
FredericRP <frederic@rolland-porche.fr>
2021-09-15 23:18:17 +0200

    PlayerCurrency: a system to load/save/convert player virtual currencies

---
+++ Runtime/Scripts/PlayerCurrencyManager.cs
@@ -0,0 +1,27 @@
+﻿using FredericRP.EventManagement;
+using FredericRP.GenericSingleton;
+using FredericRP.PersistentData;
+using UnityEngine;
+
+namespace FredericRP.PlayerCurrency
+{
+  public class PlayerCurrencyManager : Singleton<PlayerCurrencyManager>
+  {
+    [SerializeField]
+    GameEvent CurrencyUpdateEvent;//<string, int>;
+
+    // TODO : create online currency management
+
+    public virtual int GetCurrencyCount(string moneyId)
+    {
+      return PersistentDataSystem.Instance.GetSavedData<PlayerCurrencyData>().GetCount(moneyId);
+    }
+
+    public virtual int AddToCurrency(string moneyId, int count)
+    {
+      int result = PersistentDataSystem.Instance.GetSavedData<PlayerCurrencyData>().Add(moneyId, count);
+      CurrencyUpdateEvent?.Raise<string, int>(moneyId, result);
+      return result;
+    }
+  }
+}
